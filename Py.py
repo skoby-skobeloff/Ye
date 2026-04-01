757d1b63a76d730e1e72d04441ba3f76c464d48f
Skoby <128781900+skoby-skobeloff@users.noreply.github.com>
2026-04-01 19:28:32 +0500

    Update Py.py

--- Py.py
+++ Py.py
@@ -1,1 +1,11 @@
+# pip install huggingface_hub transformers
+from transformers import pipeline
 
+# text generation example
+generator = pipeline(
+    "text-generation",
+    model="meta-llama/Llama-4-Scout-17B-16E-Instruct"
+)
+
+result = generator("Hello, explain the solar system", max_new_tokens=50)
+print(result[0]['generated_text'])
