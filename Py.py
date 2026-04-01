3ddd2638f9697da34dbe920038a7d80bb13f2783
Skoby <128781900+skoby-skobeloff@users.noreply.github.com>
2026-04-01 19:51:45 +0500

    Update Py.py

--- Py.py
+++ Py.py
@@ -1,5 +1,35 @@
 # pip install huggingface_hub transformers
 from transformers import pipeline
+import discord
+from discord.ext import commands
+from discord import app_commands, message
+import os
+from sympy import sympify
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}!')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+@bot.tree.command(name="echo", description="Echoes back the message")
+@app_commands.describe(say="What should I say?")
+async def echo(interaction: discord.Interaction, say: str):
+    if isinstance(interaction.guild, discord.Guild): 
+        if "bot cmds" in interaction.user.roles or interaction.user.id == 788377795620896768:
+            await interaction.response.send_message(say)
+        else:
+            await interaction.response.send_message('missing role "bot cmds"')
+    else:
+        await interaction.response.send_message(say)
 
 # text generation example
 generator = pipeline(
